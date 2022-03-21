dfb39b0d7528f533e86d8e2aa4904990f9f4bc2c
Jonathan Chetboun <jchetboun@lightricks.com>
2022-03-21 17:52:37 +0200

    Prerun.

---
+++ prerun.sh
@@ -0,0 +1,3 @@
+wget -P checkpoint/ https://www.dropbox.com/sh/l8g9amoduz99kjh/AACGYRJj34ucfyAQkOmDniDla/stylegan2-ffhq-config-f.pt
+wget -P assets/inceptions/ https://www.dropbox.com/sh/bc8a7ewlvcxa2cf/AACbfR8bAdO7uadLRlsxLOSla/inception_ffhq_res512_50k.pkl
+wget -P assets/inceptions/ https://www.dropbox.com/sh/bc8a7ewlvcxa2cf/AAAIEwcc_r4ppBaqZPvtrrSDa/inception_ffhq_res1024_50k.pkl
